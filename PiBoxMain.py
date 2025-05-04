ed5a4538c45c6d79db7d1574ccb38ec274a5baa9
Jordan <20106285@mail.wit.ie>
2025-05-04 15:56:29 +0100

    Add basic Azure writing + A blynk button

--- PiBoxMain.py
+++ PiBoxMain.py
@@ -1,16 +1,35 @@
 from sense_hat import SenseHat
+from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
 sense = SenseHat()
 import time 
+import json
+import BlynkLib
+connection_string = "INSERT_AZURE_AUTH"
+BLYNK_TEMPLATE_ID = "TMPL4sVXjZnQO"
+BLYNK_TEMPLATE_NAME = "PiBox"
+BLYNK_AUTH_TOKEN = "INSERT_BLYNK_AUTH"
+device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
+blynk = BlynkLib.Blynk(BLYNK_AUTH_TOKEN)
+
+print("Connecting to Azure")
+try:
+    device_client.connect()
+    print("Connected!")
+except Exception as e:
+    print(f"Error connecting to the IOT hub: {e}")
 
 #Assign RGB
 GREEN = (0, 255, 0)
 RED = (255, 0 , 0)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 OFF = (0,0,0)
 
 temp = sense.get_temperature()
 humidity = sense.get_humidity()
 mag = sense.get_compass()
+
+
 
 #Define Threshold for detecting a 90-degree rotation
 ROTATION_THRESHOLD = 45
@@ -70,18 +89,38 @@ def weather_warning_hot():
         return True
     return False
 
-while True:
+def warning_signal():
+    sense.show_message("Slow Down!")
+    sense.clear(YELLOW)
+    time.sleep(2)
+    sense.clear(BLUE)
+
+
+@blynk.on("V1")
+def warning_button_handler(value):
+    if int(value[0]) == 1:
+        warning_signal()
+
+
+while True: 
     
+    blynk.run()
+
     if weather_warning_cold():
         sense.show_message("Brr!", scroll_speed=0.05, text_colour=BLUE)
         sense.clear(BLUE)
+        message = Message(json.dumps({ 'event' : 'weather_warning_cold'}))
+        device_client.send_message(message)
+        #blynk.notify("Car is very cold!")
         time.sleep(3)
 
     if weather_warning_hot():
         sense.show_message("It's Hot!", scroll_speed=0.05)
         sense.clear(RED)
+        message = Message(json.dumps({ 'event' : 'weather_warning_hot'}))
+        device_client.send_message(message)
+        #blynk.notify("Car is very hot!")
         time.sleep(3)
-
 
     if brake_check():
         sense.clear(RED)
@@ -91,6 +130,10 @@ while True:
         sense.clear(RED)
         time.sleep(0.5)
         sense.clear(BLUE)
+        message = Message(json.dumps({ 'event' : 'harsh_brake'}))
+        device_client.send_message(message)
+        #blynk.notify("Harsh movement detected!")
+        time.sleep(0.5)
 
 
     elif detect_rotation():
@@ -98,5 +141,8 @@ while True:
         sense.clear(RED)
         time.sleep(1.5)
         sense.clear(BLUE)
+        message = Message(json.dumps({ 'event' : 'rotation_detected'}))
+        device_client.send_message(message)
+        #blynk.notify("Car has flipped!")
     time.sleep(0.5)
-    
+
